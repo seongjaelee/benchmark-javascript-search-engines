782c88eea052aee0e22dcd5e3b0d35c3bc2f07b0
Seongjae Lee <seongjae@gmail.com>
2016-07-08 00:03:34 -0700

    Modify for loading 10,000 docs

--- benchmark.js
+++ benchmark.js
@@ -1,148 +1,214 @@
-console.log("Starting up")
-
 var fs = require("fs")
-var Benchmark = require('benchmark')
+var path = require("path")
+var eunr = require("elasticlunr");
 var lunr = require("lunr")
 var si = require("search-index")
+var async = require('asyncawait/async');
+var await = require('asyncawait/await');
+var zlib = require('zlib');
 
-var lunrIndexEmpty = lunr(function() {
-  this.field("filename", { boost: 10 })
-  this.field("body")
-})
-var searchIndexEmpty = si({
-  indexPath: "tmp/empty",
-  fieldsToStore: ["filename", "body"]
-})
-var suite = new Benchmark.Suite
+var filenames = fs.readdirSync(path.join(__dirname, "markdown"));
+var batchSize = 1000;
 
-console.log("Loading fixtures")
-var filenames = fs.readdirSync(__dirname + "/corpus")
-var allFileDetails = {}
-var lunrIndex = lunr(function() {
-  this.field("filename", { boost: 10 })
-  this.field("body")
-})
-var searchIndex = si({
-  indexPath: "tmp/populated",
-  fieldsToStore: ["filename", "body"]
-})
-var batch = []
-var batchOptions = {
-  fieldOptions: [
-    {fieldName: "filename", weight: 10},
-    {fieldName: "body", weight: 1}
-  ]
-}
-var indexBatch = function() {
-  var batchLength = batch.length
-  searchIndex.add(batch, batchOptions, function(err) {
-    if(!err) {
-      // console.log(batchLength + " documents indexed in batch")
-    }
-  })
-  batch = []
-}
-filenames.forEach(function(filename) {
-  var path = __dirname + "/corpus/" + filename
-
-  allFileDetails[path] = {
+function toFileDetails(filename) {
+  var filePath = path.join(__dirname, "markdown", filename);
+  fileDetails = {
     filename: filename,
-    id: path,
-    body: fs.readFileSync(path),
-    modifiedAt: fs.statSync(path).mtime.getTime()
+    id: filePath,
+    body: fs.readFileSync(filePath, 'utf8'),
+    modifiedAt: fs.statSync(filePath).mtime.getTime()
   }
-
-  lunrIndex.add(allFileDetails[path])
-
-  batch.push(allFileDetails[path])
-
-  if(batch.length == 100) {
-    indexBatch()
-  }
-})
-indexBatch()
-console.log("Fixtures loaded")
-
-var randomPath = function() {
-  return __dirname + "/corpus/" + filenames[Math.floor(Math.random() * (641 - 0 + 1))]
+  return fileDetails;
 }
 
-console.log("Starting benchmark")
-suite.add("fs#readdir", function(deferred) {
-  fs.readdir(__dirname + "/corpus", function(err, files) {
-    if(!err) {
-      deferred.resolve()
-    }
-  })
-}, {"defer": true})
-.add("fs#readFile", function(deferred) {
-  fs.readFile(randomPath(), function(err, contents) {
-    if(!err) {
-      deferred.resolve()
-    }
-  })
-}, {"defer": true})
-.add("fs#stat", function(deferred) {
-  fs.stat(randomPath(), function(err, stat) {
-    if(!err) {
-      stat.mtime.getTime()
-      deferred.resolve()
-    }
-  })
-}, {"defer": true})
-.add("lunr#add 10 documents", function() {
-  [
-    randomPath(), randomPath(), randomPath(), randomPath(), randomPath(),
-    randomPath(), randomPath(), randomPath(), randomPath(), randomPath()
-  ].forEach(function(path) {
-     var fileDetails = allFileDetails[path]
-     lunrIndexEmpty.add({
-       id: fileDetails.id,
-       filename: fileDetails.filename,
-       body: fileDetails.body
-     })
-   })
-})
-.add("search-index#add 10 documents", function(deferred) {
-   var documents = [
-     randomPath(), randomPath(), randomPath(), randomPath(), randomPath(),
-     randomPath(), randomPath(), randomPath(), randomPath(), randomPath()
-   ].map(function(path) { return allFileDetails[path] })
+function initSearchIndex() {
+  return new Promise((resolve, reject) => {
+    si({}, function(error, searchIndex) {
+      if (error) {
+        reject(error);
+        return;
+      }
+      resolve(searchIndex);
+    });
+  });
+}
 
-  searchIndexEmpty.add(documents, {}, function(err) {
-    if(!err) deferred.resolve()
-  })
-}, {"defer": true})
-.add("lunr#search", function() {
-  var results = lunrIndex.search("women")
-  if(results.length == 0) console.log("NO RESULTS FOUND")
-})
-.add("search-index#search", function(deferred) {
-  searchIndex.search({query: {"*": ["women"]}}, function(err, results) {
-    if(!err) {
-      if(results.totalHits == 0) console.log("NO RESULTS FOUND")
-      deferred.resolve()
+function close(searchIndex) {
+  return new Promise((resolve, reject) => {
+    searchIndex.close(function(error) {
+      error ? reject(error) : resolve();
+    });
+  });
+}
+
+function createBatches(batchSize) {
+  var ret = [];
+  while (filenames.length > 0) {
+    ret.push(filenames.splice(0, batchSize));
+  }
+  return ret;
+}
+
+function add(searchIndex, batch) {
+  var batchOptions = {
+    fieldOptions: [
+      {fieldName: "filename", weight: 10},
+      {fieldName: "body", weight: 1}
+    ]
+  };
+  return new Promise((resolve, reject) => {
+    searchIndex.add(batch.map(toFileDetails), batchOptions, function(error) {
+      if (error) {
+        reject(error);
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+function save(searchIndex) {
+  return new Promise((resolve, reject) => {
+    searchIndex.snapShot(function(readStream) {
+      readStream.pipe(fs.createWriteStream("backup.gz"))
+        .on('close', function() {
+          resolve();
+        });
+    });
+  });
+}
+
+function load(searchIndex) {
+  return new Promise((resolve, reject) => {
+    searchIndex.replicate(fs.createReadStream('backup.gz'), function(msg) {
+      resolve();
+    });
+  });
+}
+
+function saveLunr(lunrIndex) {
+  fs.writeFileSync(
+    path.join(__dirname, 'lunr.cache'),
+    zlib.deflateSync(JSON.stringify(lunrIndex)));
+}
+
+function loadLunr() {
+  return lunr.Index.load(JSON.parse(zlib.inflateSync(fs.readFileSync(path.join(__dirname, 'lunr.cache')))));
+}
+
+function saveEunr(eunrIndex) {
+  fs.writeFileSync(
+    path.join(__dirname, 'eunr.cache'),
+    zlib.deflateSync(JSON.stringify(eunrIndex)));
+}
+
+function loadEunr() {
+  return eunr.Index.load(JSON.parse(zlib.inflateSync(fs.readFileSync(path.join(__dirname, 'eunr.cache')))));
+}
+
+async(function () {
+  // var searchIndex = await(initSearchIndex());
+  // var batches = createBatches(batchSize);
+  // batches.forEach(function(batch, index) {
+  //   await(add(searchIndex, batch));
+  //   console.log(index * batchSize + batch.length);
+  // });
+  // console.log("added");
+  // 
+  // await(save(searchIndex));
+  // await(close(searchIndex));
+  // console.log("saved");
+  // 
+  // searchIndex = await(initSearchIndex());
+  // await(load(searchIndex));
+  // await(close(searchIndex));
+  // console.log("loaded");
+  
+  var lunrIndex = lunr(function() {
+    this.field("filename", { boost: 10 })
+    this.field("body")
+  });
+  
+  filenames.forEach(function(filename, index) {
+    lunrIndex.add(toFileDetails(filename));
+    if (index % batchSize == 0 && index != 0) {
+      console.log(index);
+    }
+  });
+  console.log("added");
+  saveLunr(lunrIndex);
+  console.log("saved");
+  lunrIndex = loadLunr();
+  console.log("loaded");
+  
+  var eunrIndex = eunr(function() {
+    this.addField("filename");
+    this.addField("body");
+    this.setRef("id");
+  });
+  filenames.forEach(function(filename, index) {
+    eunrIndex.addDoc(toFileDetails(filename));
+    if (index % batchSize == 0 && index != 0) {
+      console.log(index);
+    }
+  });
+  console.log("added");
+  saveEunr(eunrIndex);
+  console.log("saved");
+  eunrIndex = loadEunr();
+  console.log("loaded");  
+})();
+
+/*
+si({}, function(err, searchIndex) {
+  var init = Date.now();
+  var size = 0;
+  var batch = [];
+  var batchOptions = {
+    fieldOptions: [
+      {fieldName: "filename", weight: 10},
+      {fieldName: "body", weight: 1}
+    ]
+  }
+  var indexBatch = function() {
+    var batchLength = batch.length;
+    searchIndex.add(batch, batchOptions, function(err) {
+      size += batchLength;
+      console.log("" + size + "," + (Date.now() - init));
+      if (size == 10001) {
+        searchIndex.snapShot(function(readStream) {
+          readStream.pipe(fs.createWriteStream("backup.gz"))
+            .on('close', function() {
+              console.log("Saved");
+            });
+        });
+      }
+    });
+    batch = [];    
+  }
+  filenames.forEach(function(filename) {
+    var filePath = path.join(__dirname, "markdown", filename);
+
+    fileDetails = {
+      filename: filename,
+      id: filePath,
+      body: fs.readFileSync(filePath),
+      modifiedAt: fs.statSync(filePath).mtime.getTime()
+    }
+
+    batch.push(fileDetails)
+    if (batch.length == 1000) {
+      indexBatch()
     }
   })
-}, {"defer": true})
-.on("cycle", function(event) {
-  console.log(String(event.target))
-})
-.on("complete", function() {
-  console.log("Benchmark of " + Object.keys(allFileDetails).length + " documents complete")
-  searchIndexEmpty.empty(function(err) {
-    if(!err) {
-      searchIndexEmpty.close(function(err) {
-        console.log("searchIndexEmpty closed")
-      })
-    }
-  })
-  searchIndex.empty(function(err) {
-    if(!err) {
-      searchIndex.close(function(err) {
-        console.log("searchIndex closed")
-      })
-    }
-  })
-})
-.run({async: true})
+  indexBatch()
+  console.log("Fixtures loaded")
+});
+
+// var lunrIndex = lunr(function() {
+//   this.field("filename", { boost: 10 })
+//   this.field("body")
+// })
+
+// lunrIndex.add(allFileDetails[path])
+*/
